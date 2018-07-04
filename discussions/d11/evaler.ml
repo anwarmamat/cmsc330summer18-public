f245ed1295e3514fcc04e185381ff813bef64da8
Cameron Moy <mail@camoy.name>
2018-07-04 17:36:37 -0400

    Fix closure order

--- discussions/d11/evaler.ml
+++ discussions/d11/evaler.ml
@@ -9,14 +9,14 @@ and subst (t : expr) (x : var) (v : expr) : expr =
   t
 
 (* Types for closurse *)
-type closure = Closure of expr * env
+type closure = Closure of env * expr
  and env = (var * closure) list
 
 (* Evaluates expression t (with environments semantics). *)
 let rec evaler_e (t : expr) : expr =
-  let (Closure (t', _)) = evaler_e' [] t in
+  let (Closure (_, t')) = evaler_e' [] t in
   t'
 
 (* Evaluates term t in environment e. *)
 and evaler_e' (e : env) (t : expr) : closure =
-  Closure (t, e)
+  Closure (e, t)
